d00b16e602b991588084e79a6e19a13032a19ba2
vanajabantu506 <146815818+vanajabantu506@users.noreply.github.com>
2024-06-19 17:58:40 +0530

    Add files via upload

---
+++ watermelon.py
@@ -0,0 +1,11 @@
+w=int(input("enter the weight of watermelon"))
+if(w%2!=0):
+  print("no it is odd")
+else:
+  x=w/2
+if(x%2==0):
+   print("yes brother 1 gets",x,"brother2 gets",x)
+else:
+  print("yes,brother1 gets",x-1,"brother2 gets",x+1)
+      
+
